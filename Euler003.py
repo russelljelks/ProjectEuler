ace42bf0a39df7e0896631d1697c19f026aa5309
russelljelks <59517412+russelljelks@users.noreply.github.com>
2020-01-05 17:10:37 -0700

    Create Euler003.py

---
+++ Euler003.py
@@ -0,0 +1,45 @@
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+# assuming the best method to factor a number N is to count up a list of primes from 2 to sqrt(N) and test each
+
+N = 10000000001
+#N = 600851475143
+NN = int (N**0.5)
+print (N, NN)
+
+def primeList (N):
+    list = []
+    for i in range (2 ,NN + 1):
+        list.append (i)
+    
+    for i in range (len (list)):
+        if list [i]:
+            print (i, list [i])
+            for j in range (i+1, len (list)):
+                if ((list[j] / list [i]) == int (list[j] / list [i])):
+                    # print ('setting', list [j], 'to zero')
+                    list [j] = 0
+
+    finalList = []
+
+    for each in list:
+        if each: finalList.append (each)
+
+    return finalList
+                
+
+listOfPrimes = primeList (N)
+print (listOfPrimes)
+
+listOfFactors = []
+
+for each in listOfPrimes:
+    if not (N%each):
+        listOfFactors.append (each)
+        print (each)
+
+print (listOfFactors)
